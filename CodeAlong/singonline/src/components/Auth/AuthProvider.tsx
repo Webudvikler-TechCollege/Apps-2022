5d6fa33374c4c7ef4f4f7d1c5b7dcb8c16ba000e
Heinz K <heka@techcollege.dk>
2022-03-01 10:32:41 +0100

    AuthProvider Comments

--- CodeAlong/singonline/src/components/Auth/AuthProvider.tsx
+++ CodeAlong/singonline/src/components/Auth/AuthProvider.tsx
@@ -1,5 +1,9 @@
 import { useContext, useEffect, useState, createContext } from "react"
 
+/**
+ * Deklarerer interface til context object
+ * Definerer login objekt data og setter
+ */
 interface iContext {
 	loginData: {
 		user_id: number;
@@ -11,25 +15,36 @@ interface iContext {
 	setLoginData: React.Dispatch<string>;
 }
 
+// Deklarerer AuthContext objekt
 const AuthContext = createContext<iContext>({} as any)
 
+// Deklarerer function component med props.children (Indkapslede tags)
 const AuthProvider:React.FC = ({children}) => {
-	const [ loginData, setLoginData ] = useState<any>('')
 
+	// Kalder state hook til logindata
+	const [ loginData, setLoginData ] = useState<any>()
+
+	// Kalder useEffect hook til get og set af sessionstorage 
 	useEffect(() => {
-		const session_token = sessionStorage.getItem('token');
+		const session_token: string | null = sessionStorage.getItem('token');
 		if(session_token) {
 			setLoginData(JSON.parse(session_token))
 		}
-	}, [children])
+	}, 
+		// Dependency array med children - renderer hvis de ændres
+		[children]
+	)
 
 	return (
+		// Returnerer Provider med loginData og setter
 		<AuthContext.Provider value={{loginData, setLoginData}}>
 			{children}
 		</AuthContext.Provider>
 	)
 }
 
+// Deklarerer custom hook med AuthContext
 const useAuth = () => useContext(AuthContext)
 
+// Ekspoterer funktioner
 export { AuthProvider, AuthContext, useAuth }
