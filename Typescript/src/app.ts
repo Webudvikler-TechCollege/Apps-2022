3b882d1a72f040fe75df217010e0a45b9b0ffd38
Heinz K <heka@techcollege.dk>
2022-03-01 04:14:16 +0100

    Typescript Guides Update

--- Typescript/src/app.ts
+++ Typescript/src/app.ts
@@ -7,7 +7,6 @@ if (typeof userInput === "string") {
   userName = userInput;
 }
 
-
 const generateError = (message: string, code: number): never => {
 	throw { message: message, errorCode: code }
 }
